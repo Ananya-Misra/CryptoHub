fb98f3f66aec46b14f48c2d719d14357d1a3ab59
Ananya Misra <anumisra2@gmail.com>
2023-03-30 23:24:30 +0530

    more changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import {
@@ -9,35 +9,51 @@ import {
   MenuOutlined,
 } from "@ant-design/icons";
 
-import icon from '../images/crypto.png'
+import icon from "../images/crypto.png";
 
 export const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar src={icon} size="large"/>
-      
+        <Avatar src={icon} size="large" />
+
         <Typography.Title level={2} className="logo">
-            <Link to="/">CryptoHub</Link>
+          <Link to="/">CryptoHub</Link>
         </Typography.Title>
-        </div>
-       <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined/>}>
-          <Link to="/">Home</Link>
+        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
+         <MenuOutlined/>
+        </Button>
+      </div>
+      {activeMenu && (
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
           </Menu.Item>
-          <Menu.Item icon={<FundOutlined/>}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
           </Menu.Item>
-          <Menu.Item icon={<MoneyCollectOutlined/>}>
-          <Link to="/exchanges">Exchanges</Link>
+         
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
           </Menu.Item>
-          <Menu.Item icon={<BulbOutlined/>}>
-          <Link to="/news">News</Link>
-          </Menu.Item>
-
-
-       </Menu>
-    
+        </Menu>
+      )}
     </div>
   );
 };
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,41 +1,41 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoApiHeaders = { 
-'X-RapidAPI-Key': '93c0793aa6msh9661abeed4f08a8p1207e5jsnddeae6e778f6',
-'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "93c0793aa6msh9661abeed4f08a8p1207e5jsnddeae6e778f6",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
 };
-// const params={
-  
-//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//     timePeriod: '24h',
-//     'tiers[0]': '1',
-//     orderBy: 'marketCap',
-//     orderDirection: 'desc',
-//     limit: '50',
-//     offset: '0'
-// }
 
-const baseUrl= 'https://coinranking1.p.rapidapi.com'
-const createRequest=(url)=>({url, headers: cryptoApiHeaders})
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+const createRequest1 = (url) => console.log(url);
 
-export const cryptoApi=createApi({
-    reducerPath:'cryptoApi',
-    baseQuery:fetchBaseQuery({baseUrl}),
-    endpoints:(builder)=>({
-        getCryptos:builder.query(
-            {
-                query:(count)=>createRequest(`coins?limit=${count}`)
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`coins?limit=${count}`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
 
-            }
-        ),
-        getCryptoDetails:builder.query({
-            query:(coinId)=>createRequest(`/coin/${coinId}`)
-        })
+    getCryptoHistory: builder.query({
+      query: (args) => {
+        const { coinId, timePeriod } = args;
+        return {
+          // Returns url with multiple args
+          url: `/coin/${coinId}/history?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=${timePeriod}`,
+          headers: cryptoApiHeaders
+      }
+      },
+    }),
+  }),
+});
 
-    })
-}
-);
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
 
-export const{
-    useGetCryptosQuery,useGetCryptoDetailsQuery
-}= cryptoApi;
